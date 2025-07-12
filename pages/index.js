23ccafcae8e5c3b353f07e16e30b098623ac473d
Davidson Luna <davidsonluna@gmail.com>
2025-07-12 21:52:00 +0000

    configurar o editor config

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>“Lê, assim como Cristo nos amou com um amor inabalável e eterno, assim também te amo, de corpo e alma, com cada batida do meu coração, há mais de uma década e por toda a eternidade.”</h1>
+    return <h1>“Lê, assim como Cristo nos amou com um amor inabalável e eterno, assim também te amo, de corpo e alma, com cada batida do meu coração, há mais de uma década e por toda a vida.”</h1>
 }
 export default Home;
 
