fe6ddc82164d21196eeb7250b1714b3a62002adb
Davidson Luna <davidsonluna@gmail.com>
2025-07-12 22:29:14 +0000

    ádiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto dedicado ao aprimoramento do conhecimento em programação, através da implementação de um sistema end-to-end.
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
 function Home() {
-    return <h1>“Lê, assim como Cristo nos amou com um amor inabalável e eterno, assim também te amo, de corpo e alma, com cada batida do meu coração, há mais de uma década e por toda a vida.”</h1>
+  return <h1>'Frase de testes Frase de testes'</h1>;
 }
 export default Home;
-
-
-
-
-
